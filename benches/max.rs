dd455e68ad46ef8961dda49665f338b5f0ad9ad0
evenorog <evenorog@gmail.com>
2018-11-04 16:07:37 +0100

    Add create_random_vec method

--- benches/max.rs
+++ benches/max.rs
@@ -10,14 +10,19 @@ const N: usize = 100;
 const LEN: usize = 1_000_000;
 const RANGE: i32 = 1_000_000;
 
+fn create_random_vec() -> Vec<i32> {
+    let mut rng = thread_rng();
+    let mut v = Vec::with_capacity(LEN);
+    for _ in 0..LEN {
+        v.push(rng.gen_range(-RANGE, RANGE));
+    }
+    v
+}
+
 #[bench]
 fn max(b: &mut test::Bencher) {
     b.iter(|| {
-        let mut rng = thread_rng();
-        let mut v = Vec::with_capacity(LEN);
-        for _ in 0..LEN {
-            v.push(rng.gen_range(-RANGE, RANGE));
-        }
+        let mut v = create_random_vec();
         test::black_box(out::max(&mut v, N));
     });
 }
@@ -25,11 +30,7 @@ fn max(b: &mut test::Bencher) {
 #[bench]
 fn max_unstable(b: &mut test::Bencher) {
     b.iter(|| {
-        let mut rng = thread_rng();
-        let mut v = Vec::with_capacity(LEN);
-        for _ in 0..LEN {
-            v.push(rng.gen_range(-RANGE, RANGE));
-        }
+        let mut v = create_random_vec();
         test::black_box(out::max_unstable(&mut v, N));
     });
 }
@@ -37,11 +38,7 @@ fn max_unstable(b: &mut test::Bencher) {
 #[bench]
 fn sort(b: &mut test::Bencher) {
     b.iter(|| {
-        let mut rng = thread_rng();
-        let mut v = Vec::with_capacity(LEN);
-        for _ in 0..LEN {
-            v.push(rng.gen_range(-RANGE, RANGE));
-        }
+        let mut v = create_random_vec();
         v.sort();
         test::black_box(&v[..N]);
     });
@@ -50,11 +47,7 @@ fn sort(b: &mut test::Bencher) {
 #[bench]
 fn sort_unstable(b: &mut test::Bencher) {
     b.iter(|| {
-        let mut rng = thread_rng();
-        let mut v = Vec::with_capacity(LEN);
-        for _ in 0..LEN {
-            v.push(rng.gen_range(-RANGE, RANGE));
-        }
+        let mut v = create_random_vec();
         v.sort_unstable();
         test::black_box(&v[..N]);
     });
