75173b6e168de00cf375e767395ca14cfd514cc6
evenorog <evenorog@gmail.com>
2019-08-29 17:38:00 +0200

    3.1.3 -> 4.0.0

--- src/lib.rs
+++ src/lib.rs
@@ -40,7 +40,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/out/3.1.3")]
+#![doc(html_root_url = "https://docs.rs/out/4.0.0")]
 #![deny(
     bad_style,
     bare_trait_objects,
