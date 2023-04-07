18a4dd8a145b99ac89cfa52ef52c8e195d56d7a9
Even Olsson Rogstadkjærnet <evenorog@gmail.com>
2023-04-07 13:26:53 +0200

    Add more benchmarks

--- benches/rng.rs
+++ benches/rng.rs
@@ -7,7 +7,7 @@ use rand::prelude::*;
 const N: usize = 100;
 const LEN: usize = 1_000_000;
 
-fn create_random_vec() -> Vec<i32> {
+fn rng_vec() -> Vec<i32> {
     let mut rng = SmallRng::seed_from_u64(0);
     let mut v: Vec<_> = (0..LEN as i32).collect();
     v.shuffle(&mut rng);
@@ -15,11 +15,11 @@ fn create_random_vec() -> Vec<i32> {
 }
 
 mod slice {
-    use crate::{create_random_vec, N};
+    use crate::{rng_vec, N};
 
     #[bench]
     fn max(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let mut v = v.clone();
             test::black_box(out::slice::max(&mut v, N));
@@ -27,35 +27,62 @@ mod slice {
     }
 
     #[bench]
+    fn min(b: &mut test::Bencher) {
+        let v = rng_vec();
+        b.iter(|| {
+            let mut v = v.clone();
+            test::black_box(out::slice::min(&mut v, N));
+        });
+    }
+
+    #[bench]
     fn max_by_cached_key(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let mut v = v.clone();
             test::black_box(out::slice::max_by_cached_key(&mut v, N, |&a| a));
         });
     }
+
+    #[bench]
+    fn min_by_cached_key(b: &mut test::Bencher) {
+        let v = rng_vec();
+        b.iter(|| {
+            let mut v = v.clone();
+            test::black_box(out::slice::min_by_cached_key(&mut v, N, |&a| a));
+        });
+    }
 }
 
 mod iter {
-    use crate::{create_random_vec, N};
+    use crate::{rng_vec, N};
 
     #[bench]
     fn max(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let v = v.clone();
             test::black_box(out::iter::max(v, N));
         });
     }
+
+    #[bench]
+    fn min(b: &mut test::Bencher) {
+        let v = rng_vec();
+        b.iter(|| {
+            let v = v.clone();
+            test::black_box(out::iter::min(v, N));
+        });
+    }
 }
 
 mod std {
-    use crate::{create_random_vec, N};
+    use crate::{rng_vec, N};
     use std::collections::BinaryHeap;
 
     #[bench]
     fn sort(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let mut v = v.clone();
             v.sort();
@@ -65,7 +92,7 @@ mod std {
 
     #[bench]
     fn sort_unstable(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let mut v = v.clone();
             v.sort_unstable();
@@ -75,7 +102,7 @@ mod std {
 
     #[bench]
     fn sort_by_cached_key(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let mut v = v.clone();
             v.sort_by_cached_key(|&a| a);
@@ -85,7 +112,7 @@ mod std {
 
     #[bench]
     fn binary_heap(b: &mut test::Bencher) {
-        let v = create_random_vec();
+        let v = rng_vec();
         b.iter(|| {
             let heap = BinaryHeap::from(v.clone());
             test::black_box(heap.into_iter().take(N).collect::<Vec<_>>());
